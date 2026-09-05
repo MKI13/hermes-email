26242ab9045b87369db4f0df1381913abb775c30
MKI13 <marioskarampas13@gmail.com>
2026-09-05 10:58:13 +0200

    feat: route Hermes registration through profile guard

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
-"""Hermes directory-plugin entry point."""
+"""Hermes directory-plugin entry point with fail-closed profile isolation."""
 
 if __package__:
-    from .hermes_email.plugin import register
+    from .hermes_email.profile_guard import register
 else:
-    from hermes_email.plugin import register
+    from hermes_email.profile_guard import register
 
 __all__ = ["register"]
